77e49cc01e466cd4e39af029281b56ea9538b2ae
sathish-012 <sathishthangavel012@gmail.com>
2024-09-03 16:05:03 +0530

    api

--- express/main.js
+++ express/main.js
@@ -1,5 +1,8 @@
 var express =require("express");
 var app = express();
+app.use(express.json());
+
+
 app.get("/myname",(req,res)=>{
     res.json({"msg":"sathish"});
 })
@@ -15,6 +18,19 @@ app.post("/login",(req,res)=>{
     res.json({"msg":"your are correct"})
   }else{
     res.json({"msg":"you are wrong"})
+  }
+  
+})
+
+app.post("/register",(req,res)=>{
+  //let email=req['query']['email'];
+  //let pwd=req['query'] ['password'];
+  let {name,email,pwd,address}=req['body'];
+  console.log(name,email,pwd,address);
+  if (!name|| !email || !pwd ||!address){
+    res.json({"msg":"enter all details"})
+  }else{
+    res.json({"msg":"Registered"})
   }
   
 })
