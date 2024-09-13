76f4f5e72355f0259f0e11b7c1df9f2df21e80e3
sathish-012 <sathishthangavel012@gmail.com>
2024-09-13 16:06:44 +0530

    jwt

--- job_portal/main.js
+++ job_portal/main.js
@@ -11,6 +11,18 @@ const client = new MongoClient(url);
 
 const dbName = "jobportal";
 
+app.use((req,res,next)=>{
+  console.log("im middleware");
+  let {token}=req.headers;
+  if(token ==""||token == undefined){
+    res.json({"msg":"send token"})
+  }else{
+    jwt.verify(token,'SECRET');
+    next();}
+  
+});
+
+
 app.post("/create_jobs", async (req, res) => {
   let body = req.body;
   let data = {
@@ -24,14 +36,42 @@ app.post("/create_jobs", async (req, res) => {
   await db.collection("jobportal").insertOne(data);
   res.json({ msg: "Successfully created a job" });
 });
-
+app.get("/api/list_jobs")
 app.get("/list_jobs", async (req, res) => {
   await client.connect();
   let db = client.db("office");
   let data = await db.collection("jobportal").find({}).toArray();
   res.json(data);
 });
+var jwt=require("jsonwebtoken")
+app.post("/login",async(req,res)=>{
+  await client.connect();
+  let {name,password}=req.body;
+  let db =client.db("office");
+  let list =await db.collection("teachers").find({"name": name , "password":password}).toArray();
+  if(list.length>0){
+    var token =jwt.sign({name:list[0]["name"]},"SECRET");
+    res.json({"msg":"done","token":token});
+  }else{
+    res.status(400).json({"msg":"wrong"});
+  }
+  res.status(200).json(list);
+});
 
+app.post("/createTeacher", async (req, res) => {
+  let body = req.body;
+  let data = {
+    name: body["name"],
+    email: body["email"],
+    password: body["password"],
+    address: body["address"],
+    mobile_no: body["mobile_no"],
+  };
+  await client.connect();
+  let db = client.db("office");
+  await db.collection("teachers").insertOne(data);
+  res.status(200).json({ message: "Created New Teacher Record!!" });
+}); 
 // app.put("/update_job", async (req, res) => {
 //   let { name, requirements } = req.body;
 //   await client.connect();
@@ -42,7 +82,7 @@ app.get("/list_jobs", async (req, res) => {
 
 //   res.json({ msg: "Updated the requirements" });
 // });
-
+app.get("/api/update_job")
 app.put("/update_job", async (req, res) => {
   let { id, name, company, requirements } = req.body;
   await client.connect();
@@ -65,5 +105,6 @@ app.delete("/delete_job", async (req, res) => {
 
   res.json({ msg: "Deleted the Job" });
 });
+
 
 app.listen(8080);
